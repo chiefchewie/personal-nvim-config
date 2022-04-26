ee06afe484763890eae971994bcc7cd9cc617eae
Alan Tang <alan.tang.astro@gmail.com>
2022-04-26 12:39:35 -0400

    remove yaml from disabled languages

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -21,7 +21,7 @@ configs.setup {
     disable = { "" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = false,
   },
-  indent = { enable = true, disable = { "yaml" } },
+  indent = { enable = true, disable = { "" } },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,

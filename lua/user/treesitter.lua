2fb2beaf1ec8b39077a75f2912f533d586d31ed0
Alan Tang <alan.tang.astro@gmail.com>
2022-05-12 14:28:09 -0400

    add some treesitter settings and parsers

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -9,7 +9,8 @@ configs.setup {
     "cpp",
     "python",
     "java",
-    "lua"
+    "lua",
+    "javascript",
   }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
